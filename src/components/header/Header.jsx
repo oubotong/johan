9b3d43e8b94dd70c4629582995e5fccd0320890f
x0oo0x <richard97@g.ucla.edu>
2022-10-27 19:23:24 -0400

    updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/photo.gif'
 import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
@@ -13,10 +12,6 @@ const Header = () => {
         <h4 className="text-light">A Fullstack Software Developer</h4>
         <CTA />
         <HeaderSocial />
-
-        {/* <div className="me">
-          <img src={ME} alt="me" />
-        </div> */}
 
         <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
